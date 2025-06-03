7140b989c7e3b8c59d371c2a0caaf2f56aedfef9
William Gooch (Contractor) <william.gooch@004.fujifilm.com>
2025-06-03 17:12:58 -0400

    inital project setup with basic web display and Mongo connection

---
+++ README.md
@@ -0,0 +1,3 @@
+# Composer data display tool
+
+Super simple web app using vercel writen in a combo of javascript and html
---
+++ api/index.js
@@ -0,0 +1,43 @@
+// 1. Import the Express.js library
+const express = require('express');
+
+// 2. Create an Express application instance
+const app = express();
+
+// 3. Define a port number for your server to listen on
+const port = 3000; // http://localhost:3000
+
+//Mongo connection deets
+const { MongoClient } = require('mongodb');
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri);
+
+// Tell express to serve static files from the public folder
+app.use(express.static('public'));
+
+
+//API endpoint
+app.get('/api/documents', async (req, res) => {
+	try {
+		await client.connect(); //Ensure connection is Open
+		const database = client.db("ComposerData");
+		const collection = database.collection("2025Live");
+		
+		const documents = await collection.find({}).toArray();
+		//sends documents as JSON data to browser
+		res.json(documents); // res.json() automatically sends data as json
+		
+	} catch (error) {
+		console.error('Error fetching data for API:', error);
+		res.status(500).json({ message: "Error fetching data" }); //error response
+	} finally {
+	// await client.close(); //commented out
+	}
+});
+
+// 5. Start the server and make it listen for incoming requests
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+  console.log('Open your web browser and go to this address!');
+  console.log('To see the raw data, visit http://localhost:3000/api/documents');
+});
